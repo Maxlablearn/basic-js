935e1afc625352147f81d3e5d1736d957a984939
MaxLabLearn <maxlablearn@gmail.com>
2022-04-03 14:56:53 +0300

    feat: soloved What-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(inDate) {
+  if (inDate === undefined) {
+    return 'Unable to determine the time of year!'
+  };
+  if ((!(inDate instanceof Date) || Object.getOwnPropertyNames(inDate).length > 0) && arguments.length > 0) {
+    throw new Error('Invalid date!');
+  }
+  let date = new Date(inDate);
+  const seasons = {
+    0: 'winter', 1: 'winter', 11: 'winter',
+    2: 'spring', 3: 'spring', 4: 'spring',
+    5: 'summer', 6: 'summer', 7: 'summer',
+    8: 'fall', 9: 'fall', 10: 'fall',  
+  }
+  return seasons[date.getMonth()];
 }
 
 module.exports = {
