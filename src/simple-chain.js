4922cf98dd57897a9874f35cab59b82588c89458
MaxLabLearn <maxlablearn@gmail.com>
2022-04-03 21:25:57 +0300

    feat: soloved Simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+  oldChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push('' + value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (isNaN(position) || position < 1 || position > this.chain.length -1 ) {
+      console.log('---throw---  position = ', position,'   chain length = ', this.chain.length);
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!")};
+    this.chain = this.chain.slice(0, position -1).concat(this.chain.slice(position));
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.oldChain = this.chain;
+    this.chain = []
+    return `( ${this.oldChain.join(' )~~( ')} )`
   }
 };
 
