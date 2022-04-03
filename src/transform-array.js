f6f8cead164b1904a56975537968b1cd5b8e56e6
MaxLabLearn <maxlablearn@gmail.com>
2022-04-03 19:49:42 +0300

    feat: soloved Transform-arrays

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) { throw new Error("'arr' parameter must be an instance of the Array!")};
+  if (arr.length === 0) { return []}
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      if (i === arr.length) {
+        break 
+      } else {
+        i++; continue;; 
+      }
+    }
+    if (arr[i] === '--discard-prev') {
+      if (i === 0) {
+        continue;
+      } else {
+        if (arr[i-2] === '--discard-next') {
+          continue;
+        } else {
+          result.pop(); continue;
+        }
+      }
+    }
+    if (arr[i] === '--double-next') {
+      if (i === arr.length - 1) {
+        break 
+      } else {
+        result.push(arr[i+1]);
+        continue;
+      }
+    }
+    if (arr[i] === '--double-prev') {
+      if (i === 0) {
+        continue;
+      } else {
+        if (arr[i-2] === '--discard-next') {
+          continue;
+        }
+        result.push(arr[i-1]);
+        continue;
+      };
+    }
+    result.push(arr[i]);
+  }
+  return result;
 }
 
 module.exports = {
