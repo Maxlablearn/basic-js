90083d19231983739b2e998d0a9f56653f867134
MaxLabLearn <maxlablearn@gmail.com>
2022-04-27 19:18:11 +0300

    feat: soloved Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+ 
+  let result = [];
+  const count1 = options.repeatTimes || 1;
+  const count2 = options.additionRepeatTimes || 1;
+  const sep1 = options.separator === undefined? '+' : '' + options.separator;
+  const sep2 = options.additionSeparator === undefined? '|' : '' + options.additionSeparator;
+  const add = options.addition === undefined? '' : ''+options.addition;
+
+  for (let i = 0; i < count1; i++) {
+    result.push(''+str);
+
+    for (let j = 0; j < count2; j++) {
+      result.push(''+add);
+      if (j !== count2 -1) {
+        result.push(''+sep2);
+      }
+    }
+
+    if (i !== count1 - 1) {
+      result.push(''+sep1);
+    }
+  }
+
+  return result.join('')
 }
 
 module.exports = {
