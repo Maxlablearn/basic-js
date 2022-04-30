2006e9ddbb304284330cf6a8494d2806ba5c6468
MaxLabLearn <maxlablearn@gmail.com>
2022-04-30 11:22:50 +0300

    feat: add Encode line solutino

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+
+  if (!str.length) return '';
+  
+  let result = [];
+  let count = 1;
+  let letter = str[0];
+
+  for (let i = 1; i < str.length; i++) {
+    
+    if (str[i] !== letter ) {
+      count === 1? result.push(letter) : result.push(count + letter);
+      count = 1;
+      letter = str[i];
+    } else {
+      count++;
+    }
+
+    if (i === str.length - 1) {
+      count === 1? result.push(letter) : result.push(count + letter);
+    }
+  }
+
+  return result.join('');
 }
 
 module.exports = {
